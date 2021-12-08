23d4b3cd65737ea47c331b0794591930e6a0295b
SkyWa7ch3r <joel.g.w.dunstan@gmail.com>
2021-12-08 16:13:21 +0800

    Added solution for Day 8

--- Day_7/crab_submarines.py
+++ Day_7/crab_submarines.py
@@ -6,13 +6,16 @@ Expends 1 fuel for every movement horizontally
 
 '''
 
-def crab_sub_p1( file):
+def crab_sub_p1(file):
     crabs = np.array(open(file, "r").read().split(","), dtype = int)
     minimum = np.inf
     for i in range(crabs.size):
         check_min = np.sum(np.abs(crabs - i))
         if check_min < minimum:
             minimum = check_min
+            minimum_i = i
+    print(np.median(crabs))
+    print(minimum_i)
     return minimum
 
 '''
@@ -21,7 +24,7 @@ Expends more fuel for every movement horizontally
 
 '''
 
-def crab_sub_p2( file):
+def crab_sub_p2(file):
     crabs = np.array(open(file, "r").read().split(","), dtype = int)
     minimum = np.inf
     arange_sum = lambda x: np.sum(np.arange(x))
@@ -33,5 +36,26 @@ def crab_sub_p2( file):
             minimum = check_min
     return minimum
 
+'''
+Faster thanks to a suggestion from Paul Hancock
+'''
+
+def crab_sub_p2_faster(file):
+    crabs = np.array(open(file, "r").read().split(","), dtype = int)
+    minimum = np.inf
+    minimum_i = -1
+    arange_sum = lambda x: x*(x+1)/2
+    v_arange_sum = np.vectorize(arange_sum)
+    for i in range(crabs.size):
+        check_min = np.abs(crabs - i)
+        check_min = np.sum(v_arange_sum(check_min + 1))
+        if check_min < minimum:
+            minimum_i = i
+            minimum = check_min
+    print(np.mean(crabs))
+    print(minimum_i)
+    return minimum
+
 print(crab_sub_p1('input.txt'))
-print(crab_sub_p2('input.txt'))
+# print(crab_sub_p2('input.txt'))
+print(crab_sub_p2_faster('input.txt'))
